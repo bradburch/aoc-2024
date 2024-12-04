770452b8711da02d2f75ae110bf31325344032cc
Brad Burch <bradburch@duck.com>
2024-12-04 15:50:56 -0800

    day 4 part 2

--- day4/Day.java
+++ day4/Day.java
@@ -7,76 +7,108 @@ public class Day {
     
     private ArrayList<String> puzzle = new ArrayList<>();
     private static final String XMAS = "XMAS";
+    private static final String MAS = "MAS";
 
     public void part1() {
         int count = 0;
 
         for(int i = 0; i < puzzle.size(); i++) {
             for(int j = 0; j < puzzle.get(i).length(); j++) {
-                if (checkHorizontal(i, j)) ++count;
-                if (checkVertical(i, j)) ++count;
-                if (checkRightDiagonal(i, j)) ++count;
-                if (checkLeftDiagonal(i, j)) ++count;
+                if (checkHorizontal(XMAS, i, j)) ++count;
+                if (checkVertical(XMAS, i, j)) ++count;
+                if (checkRightDiagonal(XMAS, i, j)) ++count;
+                if (checkLeftDiagonal(XMAS, i, j)) ++count;
             }
         }
 
         System.out.println("XMAS Count: " + count);
-
     }
 
-    private boolean checkHorizontal(int i, int j) {
+    public void part2() {
+        int count = 0;
+
+        for(int i = 1; i < puzzle.size()-1; i++) {
+            for(int j = 1; j < puzzle.get(i).length()-1; j++) {
+                if (puzzle.get(i).charAt(j) == 'A') {
+                    if (checkRightDiagonalMiddle(MAS, i, j) && checkLeftDiagonalMiddle(MAS, i, j)) ++count;
+                }
+            }
+        }
+
+        System.out.println("X-MAS Count: " + count);
+    }
+
+    private boolean checkHorizontal(String target, int i, int j) {
         String line = puzzle.get(i);
         StringBuilder horizontal = new StringBuilder();
 
-        if (j + 3 < line.length()) {
-            horizontal.append(line.substring(j, j+4));
+        if (j + target.length()-1 < line.length()) {
+            horizontal.append(line.substring(j, j+target.length()));
         }
-
-        return checkEquality(XMAS, horizontal);
+        
+        return checkEquality(target, horizontal);
     }
 
-    private boolean checkVertical(int i, int j) {
+    private boolean checkVertical(String target, int i, int j) {
         StringBuilder vertical = new StringBuilder();
 
-        if (i + 3 < puzzle.size()) {    
-            for (int index = 0; index < 4; index++) {
+        if (i + target.length()-1 < puzzle.size()) {    
+            for (int index = 0; index < target.length(); index++) {
                 vertical.append(puzzle.get(index+i).charAt(j));
             }
         } 
 
-        return checkEquality(XMAS, vertical);
+        return checkEquality(target, vertical);
     }
 
-    private boolean checkRightDiagonal(int i, int j) {
+    private boolean checkRightDiagonal(String target, int i, int j) {
         StringBuilder rightDiagonal = new StringBuilder();
+        int len = target.length()-1;
 
-        if (i + 3 < puzzle.size() && j + 3 < puzzle.get(i).length()) {
-            for (int index = 0; index < 4; index++) {
+        if (i + len < puzzle.size() && j + len < puzzle.get(i).length()) {
+            for (int index = 0; index < target.length(); index++) {
                 rightDiagonal.append(puzzle.get(i + index).charAt(j + index));
             }
         }
 
-        return checkEquality(XMAS, rightDiagonal);
+        return checkEquality(target, rightDiagonal);
     }
 
-    private boolean checkLeftDiagonal(int i, int j) {
-        StringBuilder leftDiagonal = new StringBuilder();
+    private boolean checkRightDiagonalMiddle(String target, int i, int j) {
+        StringBuilder rightDiagonal = new StringBuilder();
 
-        if (i + 3 < puzzle.size() && j - 3 >= 0) {
-            for (int index = 0; index < 4; index++) {
+        rightDiagonal.append(puzzle.get(i-1).charAt(j-1));
+        rightDiagonal.append(puzzle.get(i).charAt(j));
+        rightDiagonal.append(puzzle.get(i+1).charAt(j+1));
+
+        return checkEquality(target, rightDiagonal);
+    }
+
+    private boolean checkLeftDiagonal(String target, int i, int j) {
+        StringBuilder leftDiagonal = new StringBuilder();
+        int len = target.length() - 1;
+
+        if (i + len < puzzle.size() && j - len >= 0) {
+            for (int index = 0; index < target.length(); index++) {
                 leftDiagonal.append(puzzle.get(i+index).charAt(j-index));
             }
         }
     
-        return checkEquality(XMAS, leftDiagonal);
+        return checkEquality(target, leftDiagonal);
+    }
+
+    private boolean checkLeftDiagonalMiddle(String target, int i, int j) {
+        StringBuilder leftDiagonal = new StringBuilder();
+
+        leftDiagonal.append(puzzle.get(i-1).charAt(j+1));
+        leftDiagonal.append(puzzle.get(i).charAt(j));
+        leftDiagonal.append(puzzle.get(i+1).charAt(j-1));
+
+        return checkEquality(target, leftDiagonal);
     }
 
     private boolean checkEquality(String word, StringBuilder puzzleWord) {
         return puzzleWord.toString().equals(word) || puzzleWord.reverse().toString().equals(word);
-    }
-
-    public void part2() {
-
     }
 
     public void parseInput(String data) {
@@ -89,5 +121,4 @@ public class Day {
 
         scanner.close();
     }
-    
 }
