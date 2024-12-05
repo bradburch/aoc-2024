f16c281be9b798fdda16de013765616d2fb7d954
Brad Burch <bradburch@duck.com>
2024-12-05 09:37:03 -0800

    day 5 part 2

--- day5/Day.java
+++ day5/Day.java
@@ -14,7 +14,29 @@ public class Day {
     private ArrayList<ArrayList<Integer>> printOrders = new ArrayList<>();
 
     public void part1() {
-        ArrayList<Integer> valid = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> valids = retrieveOrders(true);
+        int sum = 0;
+
+        for (List<Integer> valid : valids) {
+            sum += valid.get(valid.size()/2);
+        }
+
+        System.out.println("Valid: " + sum);
+    }
+
+    public void part2() {
+        ArrayList<ArrayList<Integer>> invalids = retrieveOrders(false);
+        int sum = 0;
+
+        for (List<Integer> valid : invalids) {
+            sum += valid.get(valid.size()/2);
+        }
+
+        System.out.println("Newly valid: " + sum);
+    }
+
+    private ArrayList<ArrayList<Integer>> retrieveOrders(boolean returnValid) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
         for (ArrayList<Integer> printOrder : printOrders) {
             boolean invalid = false;
@@ -24,22 +46,57 @@ public class Day {
 
                 for (int j = 0; j < pagesAfter.size(); j++) {
                     if (pagesBefore.contains(pagesAfter.get(j))) {
+                        if (!returnValid) {
+                            result.add(swap(printOrder));
+                        }
                         invalid = true;
+                        break;
                     }
                 }
 
                 if (invalid) break;
             }
-            if (!invalid) valid.add(printOrder.get(printOrder.size()/2));
+            if (returnValid && !invalid) result.add(printOrder);
         }
 
-        int sum = valid.stream().mapToInt(Integer::intValue).sum();
-
-        System.out.println("Valid: " + sum);
+        return result;
     }
 
-    public void part2() {
+    private ArrayList<Integer> swap(ArrayList<Integer> invalid) {
+        ArrayList<Integer> swapped = new ArrayList<>(invalid);
+        int[] swapIndices = getSwapIndicies(invalid);
 
+        while (swapIndices[0] != swapIndices[1]) {
+            Integer temp = swapped.get(swapIndices[0]);
+
+            swapped.set(swapIndices[0], swapped.get(swapIndices[1]));
+            swapped.set(swapIndices[1], temp);
+
+            swapIndices = getSwapIndicies(swapped);
+        }
+        
+        return swapped;
+    }
+
+    private int[] getSwapIndicies(ArrayList<Integer> printOrder) {
+        boolean invalid = false;
+        int[] swap = new int[2];
+
+        for (int i = 0; i < printOrder.size(); i++) {
+            Set<Integer> pagesBefore = orderingRules.getOrDefault(printOrder.get(i), new HashSet<>());
+            List<Integer> pagesAfter = printOrder.subList(i, printOrder.size());
+
+            for (int j = 0; j < pagesAfter.size(); j++) {
+                if (pagesBefore.contains(pagesAfter.get(j))) {
+                    swap[0] = j+i;
+                    swap[1] = i;
+                    invalid = true;
+                }
+            }
+
+            if (invalid) break;
+        }
+        return swap;
     }
 
     public void parseInput(String data) {
